dce777a585eb1d97baa4d68da7977e552f7345ae
chmod-Kishore <132202728+chmod-Kishore@users.noreply.github.com>
2024-07-24 22:07:07 +0530

    Update B.cpp

--- AtCoder/AtCoder Beginner Contest 363/B.cpp
+++ AtCoder/AtCoder Beginner Contest 363/B.cpp
@@ -1,1 +1,18 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    int n,t,p,x;
+    cin >> n >> t >> p;
+    int dp[100];
+    for(int i=0;i<n;++i){
+    	cin >>x;
+    	dp[i]=max(0,t-x);
+	}
+	sort(dp,dp+n);
+	cout << dp[p-1]<<"\n"; 
+    return 0;
+}
 
